50e489d8621f8e7691715597ae7e088664cfde56
= <=>
2022-09-12 01:00:30 +0530

    Add live feed

--- sketch.js
+++ sketch.js
@@ -1,8 +1,30 @@
+var posterWidth ;
+var posterHeight ;
+var capture ;
+
 function setup() {
-  var myCanvas = createCanvas(1080/2, 1350/2);
+  posterWidth = 1080/2;
+  posterHeight = 1350/2;
+
+  var myCanvas = createCanvas(posterWidth, posterHeight);
   myCanvas.parent("test");
+
+  capture = createCapture(VIDEO);
+  capture.size(posterWidth, posterHeight);
 }
 
 function draw() {
-  background(0);
+  clear();
+  image(capture, 0, 0, posterWidth*1.6, posterHeight);
+  filter(GRAY);
+  
+  noFill();
+  stroke(255);
+  strokeWeight(.8);
+  circle(0, posterHeight, 400);
+
+  noFill();
+  stroke(255);
+  strokeWeight(.8);
+  circle(posterWidth, 0, 600);
 }

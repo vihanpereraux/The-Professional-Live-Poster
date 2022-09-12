8159b1b899bc4d382807db564f8e79c5b8c67211
= <=>
2022-09-12 10:03:52 +0530

    Add webgl canvas

--- sketch.js
+++ sketch.js
@@ -7,28 +7,35 @@ function setup() {
   posterWidth = 1080/2;
   posterHeight = 1350/2;
 
-  var myCanvas = createCanvas(posterWidth, posterHeight);
-  myCanvas.parent("test");
+  var posterBackground = createCanvas(posterWidth, posterHeight, WEBGL);
+  posterBackground.id('poster-background');
+  posterBackground.parent("test");
 
   capture = createCapture(VIDEO);
   capture.size(posterWidth, posterHeight);
+  capture.id('live-feed')
 
   seriosuly = new Seriously();
+  var source = seriosuly.source('#live-feed');
+  var target = seriosuly.target('#poster-background')
   seriosuly.go();
 }
 
 function draw() {
-  clear();
-  image(capture, 0, 0, posterWidth*1.6, posterHeight);
-  filter(GRAY);
+  clear(); 
+  translate(-500, -340, 0); //only for webgl parameter
+  image(capture, 0, 0, posterWidth*2.3, posterHeight*1);
+  // filter(GRAY);
   
+  translate(0, 0, 0);
   noFill();
-  stroke(255);
+  stroke(255, 0, 0);
   strokeWeight(.8);
-  circle(0, posterHeight, 400);
+  circle(posterWidth, 0, 300);
 
-  noFill();
-  stroke(255);
-  strokeWeight(.8);
-  circle(posterWidth, 0, 600);
+
+  // noFill();
+  // stroke(255);
+  // strokeWeight(.8);
+  // circle(posterWidth, 0, 600);
 }

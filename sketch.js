ee41e414f0e2b97b593be5bd48d5b6aebfecefc5
= <=>
2022-09-16 10:07:21 +0530

    Add random values

--- sketch.js
+++ sketch.js
@@ -3,6 +3,10 @@ var posterHeight;
 var capture;
 var seriosuly;
 var fr;
+var layerOneRandomPosition;
+var layerOneRandomRadius;
+var layerTwoRandomPosition;
+var layerTwoRandomRadius;
 
 function setup() {
   // psoter ratios
@@ -24,25 +28,29 @@ function setup() {
 }
 
 function draw() {
-
-  // layer 01 
   clear(); 
-  drawingContext.filter = 'blur(10px)'; // applying the blur effect
-  image(capture, -200, 0, posterWidth*1.7, posterHeight*1); //without webgl params
+  
+  // layer 01 
+  drawingContext.filter = 'blur(15px)'; // applying the blur effect
+  scale(-1, 1);
+  image(capture, -800, 0, posterWidth*1.7, posterHeight*1); //without webgl params
   filter(GRAY);
   console.log(frameRate()); //debugging
   drawingContext.filter = 'none';  // removing the blur effect
 
   // layer 02 
+  scale(-1, 1);
   noFill();
   stroke(255);
   strokeWeight(2);
-  circle(0, posterHeight, 450);
+  layerOneRandomPosition = (0, windowWidth);
+  layerOneRandomRadius = (200, 500);
+  circle(random(layerOneRandomPosition), posterHeight, random(layerOneRandomRadius));
 
   // layer 03
   noFill();
   stroke(255);
   strokeWeight(2);
-  circle(posterWidth, 0, 600);
+  circle(random(posterWidth, 0), 0, random(400, 600));
   
 }

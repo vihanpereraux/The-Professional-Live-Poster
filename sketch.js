5c50affed26b16a26d64d70d64da8d9993a37d56
= <=>
2022-09-16 08:34:37 +0530

    Clean the code

--- sketch.js
+++ sketch.js
@@ -27,13 +27,12 @@ function draw() {
 
   // layer 01 
   clear(); 
-  // translate(-500, -340, 0); //only for webgl parameter
+  drawingContext.filter = 'blur(10px)'; // applying the blur effect
   image(capture, -200, 0, posterWidth*1.7, posterHeight*1); //without webgl params
-  // image(capture, 0, 0, posterWidth*2.3, posterHeight*1); //with webgl params
   filter(GRAY);
-  drawingContext.filter = 'blur(10px)';
   console.log(frameRate()); //debugging
-  
+  drawingContext.filter = 'none';  // removing the blur effect
+
   // layer 02 
   noFill();
   stroke(255);

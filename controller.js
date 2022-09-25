ab2c3ffd92bdb0efeb0b324da49e98870aa7f3f9
= <=>
2022-09-26 01:44:51 +0530

    Add keydown function to download

--- controller.js
+++ controller.js
@@ -1,12 +1,27 @@
-document.getElementById("get").onclick = function() {
-    const screenShotTarget = document.getElementById("test");
+// document.getElementById("get").onclick = function() {
+//     const screenShotTarget = document.getElementById("test");
     
-    html2canvas(screenShotTarget).then((canvas) => {
-        const base64Image = canvas.toDataURL("image/png");
-        var anchor = document.createElement('a');
-        anchor.setAttribute("href", base64Image);
-        anchor.setAttribute("download", "my-poster");
-        anchor.click();
-        anchor.remove();
-    });
-}
+//     html2canvas(screenShotTarget).then((canvas) => {
+//         const base64Image = canvas.toDataURL("image/png");
+//         var anchor = document.createElement('a');
+//         anchor.setAttribute("href", base64Image);
+//         anchor.setAttribute("download", "my-poster");
+//         anchor.click();
+//         anchor.remove();
+//     });
+// }
+
+window.addEventListener('keydown', (event) => {
+    if(event.code == "KeyD"){
+        const screenShotTarget = document.getElementById("test");
+
+        html2canvas(screenShotTarget).then((canvas) => {
+            const base64Image = canvas.toDataURL("image/png");
+            var anchor = document.createElement('a');
+            anchor.setAttribute("href", base64Image);
+            anchor.setAttribute("download", "my-poster");
+            anchor.click();
+            anchor.remove();
+        });
+    }
+});

bb9e352ca1ffeb8bf4c9780d626d5b9e24d2f94e
= <=>
2022-09-26 02:06:12 +0530

    Add comments and update funcs

--- controller.js
+++ controller.js
@@ -1,27 +1,25 @@
-// document.getElementById("get").onclick = function() {
-//     const screenShotTarget = document.getElementById("test");
-    
-//     html2canvas(screenShotTarget).then((canvas) => {
-//         const base64Image = canvas.toDataURL("image/png");
-//         var anchor = document.createElement('a');
-//         anchor.setAttribute("href", base64Image);
-//         anchor.setAttribute("download", "my-poster");
-//         anchor.click();
-//         anchor.remove();
-//     });
-// }
+// download function for download button click
+document.getElementById("get").onclick = function() {
+    convertHtml2Canvas();
+}
 
+// download function for button press event
 window.addEventListener('keydown', (event) => {
     if(event.code == "KeyD"){
-        const screenShotTarget = document.getElementById("test");
-
-        html2canvas(screenShotTarget).then((canvas) => {
-            const base64Image = canvas.toDataURL("image/png");
-            var anchor = document.createElement('a');
-            anchor.setAttribute("href", base64Image);
-            anchor.setAttribute("download", "my-poster");
-            anchor.click();
-            anchor.remove();
-        });
+        convertHtml2Canvas();
     }
 });
+
+// html2canvas function
+function convertHtml2Canvas(){
+    const screenShotTarget = document.getElementById("test");
+
+    html2canvas(screenShotTarget).then((canvas) => {
+        const base64Image = canvas.toDataURL("image/png");
+        var anchor = document.createElement('a');
+        anchor.setAttribute("href", base64Image);
+        anchor.setAttribute("download", "my-poster");
+        anchor.click();
+        anchor.remove();
+    });
+}
